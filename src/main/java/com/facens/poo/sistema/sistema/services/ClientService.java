38caf6c779e1638af324a51596c8cdde1f77076a
Manu <lopez.emanuela@gmail.com>
2021-02-23 20:21:31 -0300

    2021

---
+++ src/main/java/com/facens/poo/sistema/sistema/services/ClientService.java
@@ -0,0 +1,32 @@
+package com.facens.poo.sistema.sistema.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.facens.poo.sistema.sistema.dto.ClientDTO;
+import com.facens.poo.sistema.sistema.entities.Client;
+import com.facens.poo.sistema.sistema.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+    
+    @Autowired
+    private ClientRepository repo;
+
+    public List<ClientDTO> getClients() {
+        
+        List<Client>    list = repo.findAll();
+        List<ClientDTO> listDTO = new ArrayList<>();
+
+        for (Client c: list){
+            listDTO.add(new ClientDTO(c.getId(), c.getName()));
+            /* http://localhost:8080/h2-console/ */
+        }
+        return listDTO;
+    }
+
+}
+
